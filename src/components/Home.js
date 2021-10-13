4cdd11ebd9b1269f523771063c09d0d8d4dfc31e
Bhárbara Cechin <bharbara.cechin@gmail.com>
2021-10-13 07:12:53 -0300

    Ajustes layout para animação na Home

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,13 +6,6 @@ const Home = (props) => {
 
     return (
         <div className={`home ${selectedColor}`}>
-            <div className='info'>
-                <h1>Bhárbara Cechin</h1>
-                {/* <p>Full Stack Developer at <a href='https://www.blink.com.br/' target='_blank' rel='noreferrer'>Blynk Systems</a></p> */}
-                <p>Desenvolvedora Full Stack em <a href='https://www.blink.com.br/' target='_blank' rel='noreferrer'>Blynk Systems</a></p>
-                <a href='https://github.com/bhcechin/' target='_blank' rel='noreferrer' ><FaGithub className='icon' /></a>
-                <a href='https://www.linkedin.com/in/bhcechin/' target='_blank' rel='noreferrer' ><FaLinkedin className='icon' /></a>
-            </div>
             <div className='pattern'>
                 <svg viewBox='0 0 100 100'>
                     <defs>
@@ -23,20 +16,30 @@ const Home = (props) => {
                         <pattern id='pattern2' x='0%' y='0%' width='100%' height='100%' patternUnits='userSpaceOnUse'>
                             <circle id='second' cx='50' cy='50' r='40' />
                         </pattern>
-
-                        <pattern id='pattern3' x='0%' y='0%' width='100%' height='100%' patternUnits='userSpaceOnUse'>
-                            <path id='third' d='M25,75  Q50,95 75,75  M25,25  Q50,5 75,25' />
-                        </pattern>
-
-                        <filter id='f1' x='0' y='0'>
-                            <feGaussianBlur in='SourceGraphic' stdDeviation='1' />
-                        </filter>
                     </defs>
 
                     <rect x='0' y='0' width='100%' height='100%' fill='url(#pattern)'></rect>
                     <rect x='0' y='0' width='100%' height='100%' fill='url(#pattern2)'></rect>
+                </svg>
+            </div>
+            <div className='pattern'>
+                <svg viewBox='0 0 100 100' className='App-logo'>
+                    <defs>
+                        <pattern id='pattern3' x='0%' y='0%' width='100%' height='100%' patternUnits='userSpaceOnUse'>
+                            <path id='third' d='M25,75  Q50,95 75,75  M25,25  Q50,5 75,25' />
+                        </pattern>
+                    </defs>
+
                     <rect x='0' y='0' width='100%' height='100%' fill='url(#pattern3)'></rect>
                 </svg>
+            </div>
+
+            <div className='info'>
+                <h1>Bhárbara Cechin</h1>
+                {/* <p>Full Stack Developer at <a href='https://www.blink.com.br/' target='_blank' rel='noreferrer'>Blynk Systems</a></p> */}
+                <p>Desenvolvedora Full Stack em <br/> <a href='https://www.blink.com.br/' target='_blank' rel='noreferrer'>Blynk Systems</a></p>
+                <a href='https://github.com/bhcechin/' target='_blank' rel='noreferrer' ><FaGithub className='icon' /></a>
+                <a href='https://www.linkedin.com/in/bhcechin/' target='_blank' rel='noreferrer' ><FaLinkedin className='icon' /></a>
             </div>
         </div>
     )
